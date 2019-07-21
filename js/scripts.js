68dcc3693cba9ea12031ceed1b3af34621726767
juru-juliette <kangabejuliette@gmail.com>
2019-07-21 23:02:19 +0200

    adding an array of gender, male and female

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-var 
+function weekday(){
+    var birthday =document.getElementById("date").Value;
+    var date = new date (birthday);
+    var year = date.getfullyear();
+    console.log(year);
+    var century = (year/100)+1;
+    console.log (century);
+    var date= data.getDate();
+    console.log(date);
+    var month = getMonth();
+    month++;
+    console.log (month);
+    var male =['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+    var female=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+} 
